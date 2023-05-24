cc742f007c804f1f1e073a7822bfeb0aeab73ba6
ewen <ewenbuhot@outlook.fr>
2023-05-24 09:42:10 +0200

    push

--- src/main/java/com/example/tictactoe/GridMatch.java
+++ src/main/java/com/example/tictactoe/GridMatch.java
@@ -161,7 +161,7 @@ public void moveOfBot(){
                 moveOfExpert();
             }
         }
-}
+    }
     public void moveOfEasy(){
         if(party.getRoundPlayer2()){
             Button[] listNameButton = {button0, button1, button2, button3, button4, button5, button6, button7, button8};
